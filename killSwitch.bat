2f7f42d2f695029423207c665fe2c3f814b094a8
yremac <113031864+yremac@users.noreply.github.com>
2023-12-26 21:40:37 +0800

    Update killSwitch.bat

--- killSwitch.bat
+++ killSwitch.bat
@@ -1,4 +1,16 @@
 @echo off
+:: Check if the script is running as administrator
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" && (
+    goto :continue
+) || (
+    echo You must run this script as an administrator.
+    echo Exiting...
+    timeout /nobreak /t 3 >nul
+    exit /b
+)
+
+:continue
+chcp 65001 > nul
 setlocal enabledelayedexpansion
 
 REM Update Code 1
@@ -21,26 +33,34 @@ if %errorlevel% neq 0 (
   echo Script is up to date.
 )
 
+:mainMenu
+@echo off
+setlocal enabledelayedexpansion
+
 set RULE_BASE_NAME=BlockOutgoing
 set INTERFACE_ALLOWED=tun0
 
 :menu
 cls
-echo VPNety killSwitch Firewall Control Menu
+echo VPNety KillSwitch Firewall Control Menu
 
-echo 1. Activate killSwitch rules
-echo 2. Deactivate killSwitch rules
+echo 1. Enable KillSwitch Rules
+echo 2. Disable KillSwitch Rules
+echo 3. Check for Updates
+echo 4. Exit
 
-set /p choice=Chose action (1-2): 
+set /p choice=Choose an action (1-4): 
 
 if "%choice%"=="1" (
     call :enableFirewallRules
 ) else if "%choice%"=="2" (
     call :disableFirewallRules
 ) else if "%choice%"=="3" (
-    exit /b
+    call :checkForUpdates
+) else if "%choice%"=="4" (
+    exit /b 0
 ) else (
-    echo Invalid input.  Please select 1 or 2.
+    echo Invalid input. Please choose 1, 2, 3, or 4.
     timeout /nobreak /t 3 >nul
     goto :menu
 )
@@ -48,25 +68,44 @@ if "%choice%"=="1" (
 :enableFirewallRules
 echo Enabling rules...
 
-:: Получаем список интерфейсов и создаем правила
+:: Get the list of interfaces and create rules
 for /f "tokens=*" %%I in ('powershell -Command "Get-NetAdapter | Where-Object { $_.InterfaceAlias -ne '!INTERFACE_ALLOWED!' } | ForEach-Object { $_.InterfaceAlias }"') do (
     powershell -Command "New-NetFirewallRule -DisplayName '%RULE_BASE_NAME%_%%I' -Direction Outbound -Action Block -InterfaceAlias '%%I'" >nul 2>&1
-    echo The rule for interface %%I is enabled.
+    echo Rule for interface %%I enabled.
 )
 
-echo All rules included.
+echo All rules are enabled.
 timeout /nobreak /t 3 >nul
 goto :menu
 
 :disableFirewallRules
 echo Disabling rules...
 
-:: Получаем список интерфейсов и удаляем правила
+:: Get the list of interfaces and remove rules
 for /f "tokens=*" %%I in ('powershell -Command "Get-NetAdapter | Where-Object { $_.InterfaceAlias -ne '!INTERFACE_ALLOWED!' } | ForEach-Object { $_.InterfaceAlias }"') do (
     powershell -Command "Remove-NetFirewallRule -DisplayName '%RULE_BASE_NAME%_%%I'" 2>nul
-    echo The rule for interface %%I is disabled.
+    echo Rule for interface %%I disabled.
 )
 
 echo All rules are disabled.
+timeout /nobreak /t 3 >nul
+goto :menu
+
+:checkForUpdates
+REM Update Code 2
+echo Checking for updates...
+
+powershell -command "& { Invoke-WebRequest -Uri '%repoURL%' -OutFile '%tempFile%' }"
+
+fc /b %0 %tempFile% > nul
+
+if %errorlevel% neq 0 (
+  echo New version of the script found. Updating...
+  move /y %tempFile% %0
+  echo Update completed.
+) else (
+  echo Script is up to date.
+)
+
 timeout /nobreak /t 3 >nul
 goto :menu
